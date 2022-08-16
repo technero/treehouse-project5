cb4f59b88ac4cd0b103744336f6808347dbe68d1
technero <nermeen.hamed86@gmail.com>
2022-08-15 22:35:55 -0700

    put final touch in css

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 baguetteBox.run('.gallery');
 
+const search = new Filter('search', 'data-caption');
+
+//document.getElementById("search").addEventListener("keyup", myFunction);
+
+//function myFunction(event) {
+// console.log(event);
+//let x = document.getElementById("search");
+//const searchValue = x.value.toLowerCase();
+//console.log(searchValue);
+//document.querySelectorAll('a')
+
